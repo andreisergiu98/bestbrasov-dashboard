c7ab5997bd57f339314eddb470f22d0f8ffa4b2c
Andrei Pampu <pampu.s.andrei@gmail.com>
2021-09-11 13:15:58 +0300

    feat(frontend): enable typechecking in development

--- frontend/components/sidebar/sidebar-items.tsx
+++ frontend/components/sidebar/sidebar-items.tsx
@@ -1,33 +1,12 @@
-import { ReactNode } from 'react';
-import { NavLink } from 'react-router-dom';
-import { Divider, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { Divider, List } from '@material-ui/core';
 import {
 	HomeRounded,
 	PeopleRounded,
 	EventRounded,
 	BusinessRounded,
 } from '@material-ui/icons';
+import { SidebarItem } from './sidebar-item';
 import { createRoute } from '../../routes';
-
-interface ItemProps {
-	to: string;
-	title: string;
-	icon?: ReactNode;
-}
-
-function SidebarItem(props: ItemProps) {
-	return (
-		<ListItem
-			button={true}
-			component={NavLink}
-			to={props.to}
-			activeClassName="Mui-selected"
-		>
-			<ListItemIcon>{props.icon}</ListItemIcon>
-			<ListItemText primary={props.title} />
-		</ListItem>
-	);
-}
 
 export function SidebarItems() {
 	return (
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+import checker from 'vite-plugin-checker';
 import graphql from '@rollup/plugin-graphql';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default defineConfig({
-	plugins: [svgr(), graphql(), reactRefresh()],
+	plugins: [svgr(), graphql(), reactRefresh(), checker({ typescript: true })],
 	esbuild: {
 		jsxInject: "import React from 'react';",
 	},
