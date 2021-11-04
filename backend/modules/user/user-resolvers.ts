657749c56edd1e18ac774e57afd412c4bee33fca
Andrei Pampu <pampu.s.andrei@gmail.com>
2021-11-04 21:22:49 +0200

    fix(backend): allow guests to see emails

--- backend/modules/user/user-resolvers.ts
+++ backend/modules/user/user-resolvers.ts
@@ -1,25 +1,13 @@
 import {
-	applyModelsEnhanceMap,
 	FindFirstUserResolver,
 	FindManyUserResolver,
 	FindUniqueUserResolver,
 } from '@lib/resolvers';
-import { UseRule } from '@lib/rule';
-import { hasBasicRights } from '@rules/user';
 import { MeResolver } from './resolvers/me-resolver';
 import { UserProfileResolver } from './resolvers/user-profile-resolver';
 import { UserRoleResolver } from './resolvers/user-role-resolver';
 import { UserSessionResolver } from './resolvers/user-session-resolver';
 import { UserSubscriptions } from './subscriptions/user-subscriptions';
-
-applyModelsEnhanceMap({
-	User: {
-		fields: {
-			email: [UseRule(hasBasicRights())],
-			phoneNumber: [UseRule(hasBasicRights())],
-		},
-	},
-});
 
 export const userResolvers = [
 	MeResolver,
